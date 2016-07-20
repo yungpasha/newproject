93ad3e6d780d9a053d05ea915af754cc8d013d59
yungpasha <blacktea322@gmail.com>
2016-07-20 12:04:08 +0300

    Add files via upload

---
+++ sketch_jul20a.ino
@@ -0,0 +1,15 @@
+int n, m;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  n = analogRead(A0);
+  analogWrite(11, 255);
+  delay(n);
+  analogWrite(11, 0);
+  delay(n);
+}
