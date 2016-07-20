9bfc3af686332ad3bab23be501891a790bb6a715
yungpasha <blacktea322@gmail.com>
2016-07-20 13:11:36 +0300

    Add files via upload

---
+++ sketch_jul20a1.ino
@@ -0,0 +1,18 @@
+ 
+void setup() 
+{     
+        pinMode(6, OUTPUT);
+        pinMode(7, OUTPUT);  
+} 
+ 
+void loop() 
+{ 
+  
+  for (int i = 0; i < 256; i++){
+    analogWrite(6, i);
+    digitalWrite(7, LOW);
+    delay(10);
+  }
+  while (true)
+    ;    
+}
