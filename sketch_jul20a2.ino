d1436d9ec4873ff6d8197b0544ddd3924cd6c991
yungpasha <blacktea322@gmail.com>
2016-07-20 15:59:53 +0300

    Add files via upload
    не распознает значения

---
+++ sketch_jul20a2.ino
@@ -0,0 +1,21 @@
+int n; 
+void setup() 
+{     
+        pinMode(4, OUTPUT);
+        pinMode(5, OUTPUT);
+        pinMode(A0, INPUT);
+} 
+ 
+void loop() 
+{ 
+  n = analogRead(A0);
+  if(n > 510){
+      analogWrite(4, 126);
+      digitalWrite(5, LOW); 
+  }
+ else{
+  analogWrite(4, 0);
+  digitalWrite(5, LOW);
+   
+ }
+}
